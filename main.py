60525cdf57229d5e3eeac4edd9f3a67309887e61
Username <111523340+BunyaphonCh@users.noreply.github.com>
2026-02-26 22:40:19 +0700

    upload info lab7

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-def summation(n):
-    return n * (n+1) // 2
+import json
+def intersact(a,b,c):
+    set_a = set(a)
+    set_b = set(b)
+    set_c = set(c)
+    
+    for item in set_a:
+        if item in set(b) and item in set(c):
+            return True
+    return False
+def main():
+    lis_a = json.loads(input())
+    lis_b = json.loads(input())
+    lis_c = json.loads(input())
+    print(intersact(lis_a, lis_b, lis_c))
+main()
