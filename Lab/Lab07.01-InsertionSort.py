361f326819cc360505baa8a1abb79605b390e9f7
Username <111523340+BunyaphonCh@users.noreply.github.com>
2026-02-27 02:08:07 +0700

    wtf code

---
+++ Lab/Lab07.01-InsertionSort.py
@@ -0,0 +1,15 @@
+def insertionSort(lst, last):
+    comparison = 0
+    for i in range(1, last + 1):
+        current_value = lst[i]
+        j = i - 1
+        while j >= 0:
+            comparison += 1
+            if lst[j] > current_value:
+                lst[j+1] = lst[j]
+                j -= 1
+            else:
+                break
+        lst[j + 1] = current_value
+        print(lst)
+    print("Comparison times:", comparison)
