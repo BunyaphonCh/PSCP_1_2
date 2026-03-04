c8fe06bdb0522f5529fec3fcff418bbac9b4ec22
Username <111523340+BunyaphonCh@users.noreply.github.com>
2026-03-04 07:53:10 +0700

    bye lab8_2

--- Efficiency/Labs08.02-ItemClass.py
+++ Efficiency/Labs08.02-ItemClass.py
@@ -1,25 +1,14 @@
-'''
-1. สร้าง class
-2. สร้าง __init__ กำหนด attribute โดยใช้ self.x = x
-3. สร้าง method get บลาๆ อย่าลืมใส่ parameter self กับต้อง return'''
+'''ไม่ได้สร้าง attribute เเต่ไปดึงตรง main เเทนเลย'''
 
 class Item:
-    def __init__(self,name, price, weight):
+    def __init__(self, name, price, weight):
         self.name = name
         self.price = price
         self.weight = weight
-    def get_name(self):
-        return self.name
-    def get_price(self):
-        return self.price
-    def get_weight(self):
-        return self.weight
-    def get_cost(self):
-        return self.price
 
 def main():
     import json
     item_in = json.loads(input())
-    item = Item(item_in["name"], item_in["price"], item_in["weight"])
-    print(item.get_name(), item.get_price(), item.get_weight(), sep='\n')
+    item = Item(item_in['name'], item_in['price'], item_in['weight'])
+    print(item.name, item.price, item.weight, sep='\n')
 main()
